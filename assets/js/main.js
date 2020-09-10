9949b17622f5650e3b784663cb62d5fde2cf2a37
Oscar Giraldo <oscardan@utp.edu.co>
2020-09-10 01:45:37 -0500

    hader nav nav2 section-paralax diseño y colores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop() > 0) {
-            $('header').addClass('header2');
+            $('nav').addClass('nav2');
         }else{
-            $('header').removeClass('header2');
+            $('nav').removeClass('nav2');
         }
     });
 });
